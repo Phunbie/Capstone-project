8eda9672d960c9401e29e99b36cc61d729b31ad1
Phunbie <adeneyeoluwafunbi@gmail.com>
2023-01-17 10:14:01 +0100

    add rest api views

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -1,14 +1,11 @@
 from django.db import models
-
 # Create your models here.
-class  Menu(models.Model):
-    id = models.IntegerField(max_length=5, primary_key=True)
+class Menu(models.Model):
     title = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    inventory = models.IntegerField(max_length=5)
+    inventory = models.IntegerField()
 
 class Booking(models.Model):
-    id = models.IntegerField(max_length=11, primary_key=True)
     name = models.CharField(max_length=255)
-    no_of_guest = models.IntegerField(max_length=6)
+    no_of_guest = models.IntegerField()
     bookingDate = models.DateTimeField()
